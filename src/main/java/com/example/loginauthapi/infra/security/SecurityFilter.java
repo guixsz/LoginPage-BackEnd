5c9e081d0661a4c67f4dfd88d74a8579c8bccb01
Guilherme Santana <guiguilsantana@gmail.com>
2024-04-05 10:00:19 -0300

    feat: creating SecurityFilter and filterInternal

--- src/main/java/com/example/loginauthapi/infra/security/SecurityFilter.java
+++ src/main/java/com/example/loginauthapi/infra/security/SecurityFilter.java
@@ -8,13 +8,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Collections;
 
 @Component
